f0aeee47ae22562dc3448d770dd171755d4a9443
Tushar Sahu <72395965+tushar-ksolves@users.noreply.github.com>
2020-10-08 00:17:45 +0530

    Add files via upload
    Day 6 extended work...

---
+++ Day_6_continued/task-manager/src/models/user.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const User = mongoose.model('User', {
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid');
+            }
+        }
+    },
+    age: {
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be +ve');
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 7,
+        validate(value) {
+            if (value.toLowerCase().includes('password')) {
+                throw new Error('Password field should not include password');
+            }
+        }
+
+    }
+})
+
+module.exports = User;
